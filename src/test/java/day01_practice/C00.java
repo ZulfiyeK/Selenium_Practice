cbb77f0c1cbfe75ab353b2289684c05b57f2a583
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-04 20:19:39 +0300

    duzenleme

--- src/test/java/day01_practice/C00.java
+++ src/test/java/day01_practice/C00.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 public class C00 {
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver","src/resource/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/Resources/Drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
