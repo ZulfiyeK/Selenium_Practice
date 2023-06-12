531fd33c7b54b67b953b63571e2ac53eeaf7d7f8
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-13 00:14:09 +0300

    util

--- src/test/java/day04_Options/C03_PageSource.java
+++ src/test/java/day04_Options/C03_PageSource.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C03_KaynakKodlari {
+public class C03_PageSource {
     static WebDriver driver;
 
     @Before
