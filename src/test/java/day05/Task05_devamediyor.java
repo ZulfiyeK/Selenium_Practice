1fd045a29afb03aa454ba418e218c8db0c83d9dd
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-14 21:02:09 +0300

    duzeltme

--- src/test/java/day05/Task05_devamediyor.java
+++ src/test/java/day05/Task05_devamediyor.java
@@ -2,7 +2,7 @@ package day05;
 
 import org.junit.Test;
 
-public class Task05 {
+public class Task05_devamediyor {
     @Test
     public void task05() {
       // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
