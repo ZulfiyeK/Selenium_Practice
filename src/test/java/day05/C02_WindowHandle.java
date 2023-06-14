1d6a204734918b9fd313f3361481679d29a9bce9
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-14 23:36:56 +0300

    Iframe

---
+++ src/test/java/day05/C02_WindowHandle.java
@@ -0,0 +1,55 @@
+package day05;
+
+import org.apache.hc.core5.http.nio.support.AbstractServerExchangeHandler;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import util.Methots;
+
+public class C02_WindowHandle extends Methots {
+
+    @Test
+    public void test() {
+
+    // 1- https://www.amazon.com sayfasina gidelim
+        driver.get("https://www.amazon.com");
+        String amazonHandle = driver.getWindowHandle();
+
+
+    // 2- url'in 'amazon' icerdigini test edelim
+        Assert.assertTrue(driver.getCurrentUrl().contains("amazon"));
+
+
+    // 3- yeni bir pencere acip https://www.bestbuy.com sayfasina gidelim
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get(" https://www.bestbuy.com");
+        String bestbuyHandle = driver.getWindowHandle();
+
+
+    // 4- title'in 'Best Buy' icerdigini test edelim
+        String bestbuyTitle = driver.getTitle();
+        Assert.assertTrue(bestbuyTitle.contains("Best Buy"));
+
+    // 5- ilk sayfaya(amazon) donup sayfada java aratalım
+        driver.switchTo().window(amazonHandle);
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java"+ Keys.ENTER);
+
+
+    // 6- arama sonuclarının 'Java' icerdigini test edelim
+        WebElement aramaSonucu = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(aramaSonucu.getText().contains("Java"));
+
+    // 7- ikinci sayfaya(bestbuy) donelim
+        driver.switchTo().window(bestbuyHandle);
+
+
+    // 8- logonun gorundugunu test edelim
+        WebElement logo = driver.findElement(By.xpath("(//img[@class='logo'])[1]"));
+        Assert.assertTrue(logo.isDisplayed());
+
+
+
+
+
+    }
+}
---
+++ src/test/java/day05/C03_Iframe.java
@@ -0,0 +1,55 @@
+package day05;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import util.Methots;
+
+import javax.swing.*;
+
+public class C03_Iframe extends Methots {
+    @Test
+    public void test() {
+
+      // https://html.com/tags/iframe sayfasına gidiniz
+        driver.get("https://html.com/tags/iframe");
+
+
+      // Videoyu görecek kadar asagiya ininiz
+        Actions actions = new Actions(driver);//sayfa icinde hareket edebilmek icin actions objesi olusturup,
+                                              // sendKeys(Keys.PAGE_DOWN) ile sayfayi asagi kaydiriyoruz.
+                                              // gerekirse sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN) seklinde tekrar kullanilir.
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();//action isleminin bittigini perform() ile belirtiyoruz.
+
+
+      // Videoyu izlemek icin Play tusuna basiniz
+        WebElement iframe = driver.findElement(By.xpath("(//iframe)[1]"));
+        driver.switchTo().frame(iframe);
+        WebElement play = driver.findElement(By.xpath("//*[@class ='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']"));
+        play.click();
+
+        /*
+         Play'i dogru locate edip click yapmamıza ragmen videoyu calıstırmadı bunun üzerine HTML kodlarını inceleyince
+         play'in aslında bir iframe icerisinde oldugunu gördük. Bu durumda once iframe'i locate edip switchTo() ile
+         iframe'e gecmeliyiz
+        */
+
+
+      // Videoyu calistirdiginizi test ediniz
+        WebElement youTubeYazisi = driver.findElement(By.xpath("//a[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']"));
+        Assert.assertTrue(youTubeYazisi.isDisplayed());
+
+
+      // 'Powerful,but easy to misuse' yazısının gorunur oldugunu test ediniz
+        driver.switchTo().parentFrame();
+
+        WebElement powerYazisi = driver.findElement(By.xpath("//*[@id='Powerful_but_easy_to_misuse']"));
+        Assert.assertTrue(powerYazisi.isDisplayed());
+
+
+    }
+}
